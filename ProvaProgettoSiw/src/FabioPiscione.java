7c5bc79f4ae7058441036c210944139b4cc3b072
Daniele Giunta <dani.giunta94@gmail.com>
2017-05-22 10:53:56 +0200

    ciaoooo 2

--- ProvaProgettoSiw/src/FabioPiscione.java
+++ ProvaProgettoSiw/src/FabioPiscione.java
@@ -3,6 +3,16 @@ public class FabioPiscione {
 
 	public FabioPiscione() {
 		// TODO Auto-generated constructor stub
+		
+	}
+	
+	public static void main(String [] args) {
+		FabioPiscione();
+	}
+
+	private static void FabioPiscione() {
+		// TODO Auto-generated method stub
+		System.out.println("DioPorco");
 	}
 
 }
